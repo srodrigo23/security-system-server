5ebb649b69e068e5217da94ea706a68d9a3d0c11
srodrigo23 <rodrigosergio93@gmail.com>
2023-02-20 22:25:01 -0400

    add google drive support

--- src/whatsapp/whatsapp_sender.py
+++ src/whatsapp/whatsapp_sender.py
@@ -14,6 +14,7 @@ def send_message(message_body:str)->None:
     message = client.messages.create(
         body=message_body,
         # media_url='https://raw.githubusercontent.com/dianephan/flask_upload_photos/main/UPLOADS/DRAW_THE_OWL_MEME.png',
+        media_url='https://ik.imagekit.io/srodrigo23/test/241188806_1013240782576434_1846265590439094677_n.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1676909762705',
         from_=from_whatsapp_number,
         to=to_whatsapp_number
     )
