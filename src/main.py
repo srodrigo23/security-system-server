8092e0f4ce79392c41fc9bcd19dab8afdeca2b37
srodrigo23 <rodrigosergio93@gmail.com>
2023-04-15 19:31:07 -0400

    add server controller

--- src/main.py
+++ src/main.py
@@ -6,9 +6,11 @@ from view.main_screen import MainScreen
 from view.main_screen import Controller
 
 if __name__ == "__main__":
-    controller = Controller() 
+    controller = Controller()
     main_screen = MainScreen(controller=controller)
+    controller.set_view(view=main_screen)
     main_screen.mainloop()
+    
 
     # tcp_server = TCPServer()
     # tcp_server.prepare_server()
--- src/view/main_screen.py
+++ src/view/main_screen.py
@@ -1,33 +1,44 @@
 
 import tkinter as tk
-
 from tkinter import *
-    # from tkinter.ttk import Button
 
 class ControlPanel(LabelFrame):
 
     def __init__(self, parent, controller)-> None:
         LabelFrame.__init__(self=self, master=parent)
         self.message = "Control Panel"
-        self.controller = controller 
+        self.controller = controller
+        self.is_on = False
         self.setup()
+        # self.setup_swith_button()
 
     def setup(self)-> None:
         self.config(text=self.message)
 
         self.columnconfigure(0, weight=1)
         self.columnconfigure(1, weight=1)
-        self.columnconfigure(2, weight=1)
 
-        label_execution =  Label(
+        self.swith_server_button = Button(
             master=self, 
-            text="Execution")
-        label_execution.grid(
-            row=0, column=0, 
+            compound='left',
+            text="Server On",
+            command=lambda:self.controller.manage_server()
+        )
+        self.swith_server_button.grid(
+            row=0, column=0,
             rowspan=1, columnspan=1, 
             padx=2, pady=2, 
             sticky='ew'
         )
+        # label_execution =  Label(
+        #     master=self, 
+        #     text="Execution :")
+        # label_execution.grid(
+        #     row=0, column=0, 
+        #     rowspan=1, columnspan=1, 
+        #     padx=2, pady=2, 
+        #     sticky='ew'
+        # )
 
         config_button = Button(
             master=self, 
@@ -36,27 +47,12 @@ class ControlPanel(LabelFrame):
             command=lambda:self.controller.make_something()
         )
         config_button.grid(
-            row=0, column=2,
-            rowspan=1, columnspan=1, 
-            padx=2, pady=2, 
-            sticky='ew'
-        )
-
-        swith_server = Button(
-            master=self, 
-            compound='left',
-            text="Configuration",
-            command=lambda:self.controller.make_something()
-        )
-
-        swith_server.grid(
             row=0, column=1,
             rowspan=1, columnspan=1, 
             padx=2, pady=2, 
             sticky='ew'
         )
-    def swith_button(self)->None:
-        pass
+        # self.swith_server.config(command=lambda:swith_button(is_on=self.is_on,button=self.swith_server))
 
 
 class MainScreen(tk.Tk):
@@ -65,7 +61,7 @@ class MainScreen(tk.Tk):
         super().__init__()
         self.width = 500
         self.height = 500
-        self.isResizable = False
+        self.isResizable = True
         self.controller  = controller
         self.maintitle = "Server"
         self.setup()
@@ -76,18 +72,21 @@ class MainScreen(tk.Tk):
     def setup(self) -> None:
         self.title(self.maintitle)
         self.geometry(f'{self.width}x{self.height}')
-        # self.resizable(self.isResizable, self.isResizable)
+        self.resizable(self.isResizable, self.isResizable)
         # self.columnconfigure(0, weight=1)
         self.columnconfigure(1, weight=1)
         # self.columnconfigure(2, weight=1)
         # self.rowconfigure(0, weight=0)
     
     def setup_control_panel(self) -> None:
-        control_panel = ControlPanel(
+        self.control_panel = ControlPanel(
             parent=self,
-            controller=self.controller
+            controller=self.controller.server_controller
         )
-        control_panel.grid(row=0, column=0, rowspan=1, columnspan=4, padx=5, pady=5, sticky='new')
+        self.control_panel.grid(
+            row=0, column=0, 
+            rowspan=1, columnspan=4, 
+            padx=5, pady=5, sticky='new')
     
     def setup_exit_menu(self)->None:
         self.__menubar__ = tk.Menu(self)
@@ -95,26 +94,38 @@ class MainScreen(tk.Tk):
         file_menu = tk.Menu(self.__menubar__)
         file_menu.add_command(label='Exit', command=self.destroy)
         self.__menubar__.add_cascade(label='File', menu=file_menu)
-        self.resizable(False, False)
 
             
 class Controller:
+
     def __init__(self) -> None:
-        pass
+        self.view = None
+        self.server_controller = ServerController()
 
     def make_something(self) -> None:
         print('Damas gratis!!')
 
+    def set_view(self, view) ->None:
+        self.view = view
+        self.server_controller.set_view(view=view.control_panel)
 
-class ToggleButton(Button):
 
-    def __init__(self, parent, function) -> None:
-        super().__init__(self=self, master=parent, command=function)
-        # (self=self, master=parent)
-        self.image_on = PhotoImage(file = "../img/on.png")
-        self.image_off = PhotoImage(file = "../img/off.png")
-        self.isOn = False
 
-    def switch(self) -> None:
-        pass
-        # self.config(image=)
+class ServerController():
+
+    def __init__(self) -> None:
+        self.is_on = False
+        self.view = None
+
+    def set_view(self, view) -> None:
+        self.view = view
+
+    def manage_server(self) ->None:
+        if self.is_on:
+            print('apagar')
+            self.view.swith_server_button.config(text="Server On")
+            self.is_on = False
+        else:
+            print('encender')
+            self.view.swith_server_button.config(text="Server Off")
+            self.is_on = True
