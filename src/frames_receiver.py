dfcef2a54b8654d2e67200fcf1b75dc9089c1837
srodrigo23 <rodrigosergio93@gmail.com>
2023-04-29 19:09:51 -0400

    another fix

--- src/frames_receiver.py
+++ src/frames_receiver.py
@@ -1,14 +1,19 @@
 """
-Class to receive frames from socket connection
+Author  : Sergio Rodrigo Cardenas Rivera
+Email   : rodrigosergio93@gmail.com
+Version : 1.0
+GitHub  : @srodrigo23
 """
+
 from threading import Thread
 import struct
 import pickle
 import cv2
+import socket as skt
 
 class FramesReceiver(Thread):
     """
-    Thread class to frames receiver
+    Class to receive frames from socket connection
     """
     def __init__(self, connection):
         Thread.__init__(self)
@@ -42,9 +47,11 @@ class FramesReceiver(Thread):
                     self.data += data
                 else:
                     self.stop_connection()
-            except Exception as e:
-                print(f"{e} error in get message size")
+            except skt.error as error:
+                print(f"{error} error in get message size")
                 self.stop_connection()
+            except KeyboardInterrupt:
+                print('i', "Error getting images")
         if self.running:
             packed_msg_size = self.data[:self.payload_size] # receive image row data form client socket
             self.data = self.data[self.payload_size:]
@@ -58,24 +65,27 @@ class FramesReceiver(Thread):
         """
         msg_size = self.get_message_size()
         if msg_size != 0:
-            while len(self.data) < msg_size and self.running:
-                try:
-                    data = self.conn.recv(4096)
-                    if len(data) > 0:
-                        self.data += data
-                    else:
+            try:
+                while len(self.data) < msg_size and self.running:
+                    try:
+                        data = self.conn.recv(4096)
+                        if len(data) > 0:
+                            self.data += data
+                        else:
+                            self.stop_connection()
+                    except Exception as e:
+                        print(f"{e} error in read frame")
                         self.stop_connection()
-                except Exception as e:
-                    print(f"{e} error in read frame")
-                    self.stop_connection()
-            if self.running:
-                frame_data = self.data[:msg_size]
-                self.data = self.data[msg_size:]
-                frame = pickle.loads(frame_data, fix_imports=True, encoding="bytes")
-                return cv2.imdecode(frame, cv2.IMREAD_COLOR)
-            else:
-                print("Returning none")
-                return None
+                if self.running:
+                    frame_data = self.data[:msg_size]
+                    self.data = self.data[msg_size:]
+                    frame = pickle.loads(frame_data, fix_imports=True, encoding="bytes")
+                    return cv2.imdecode(frame, cv2.IMREAD_COLOR)
+                else:
+                    print("Returning none")
+                    return None
+            except KeyboardInterrupt:
+                print("there was an error")
         else:
             return None
         
--- src/tcp_server.py
+++ src/tcp_server.py
@@ -1,36 +1,40 @@
 """
-Code to run a server to manage socket connections
+Author  : Sergio Rodrigo Cardenas Rivera
+Email   : rodrigosergio93@gmail.com
+Version : 1.0
+GitHub  : @srodrigo23
 """
+
 import socket as skt
 import uuid
-import settings as s
 import random
-
+import settings as s
 from util.logger import print_log
 from connection import Connection
-# from util.date import get_date
-# from util.date import get_time
+
 HOST = s.get_host()
-# PORT = int(s.get_port())
 PORT = random.randint(49152, 65535)
 
 NUMBER_CONNECTIONS = 10
-
 class TCPServer:
     """
-    Server to manage socket connections
+    Code to run a server to manage socket connections
     """
-    def __init__(self, server_mode:str):
+
+    def __init__(self, server_mode:str) -> None:
         """
         Method to init a TCPServer class from host and port.
-        """ 
+        """
         self.socket             = None
         self.tcp_server_running = True
         self.connections        = {}    # to store every Connection object
         self.id_cameras         = set() # id cameras
         self.define_server_mode(server_mode)
-    
-    def define_server_mode(self, actual_mode:str)-> None:
+
+    def define_server_mode(self, actual_mode:str) -> None:
+        """
+        Method to turnOn detectors by command line argumnets
+        """
         if actual_mode is not None:
             mode = actual_mode.lower()
             if mode =="f":
@@ -39,6 +43,7 @@ class TCPServer:
                 s.set_motion_detector_status(enabled="false")
                 s.set_people_detector_status(enabled="false")
                 s.set_stream_enabled(enabled="false")
+
             elif mode == "m":
                 s.set_motion_detector_status(enabled="true")
 
@@ -51,25 +56,20 @@ class TCPServer:
 
                 s.set_motion_detector_status(enabled="false")
                 s.set_fire_detector_status(enabled="false")
-                s.set_stream_enabled(enabled="false")
-            
+                s.set_stream_enabled(enabled="false")            
             elif mode == "s":
                 s.set_stream_enabled(enabled="true")
 
                 s.set_people_detector_status(enabled="false")
                 s.set_motion_detector_status(enabled="false")
                 s.set_fire_detector_status(enabled="false")
-                
-        
         self.status_stream = s.get_stream_enabled()
         self.status_fire_detector = s.get_fire_detector_status()
         self.status_motion_detector = s.get_motion_detector_status()
         self.status_people_detector = s.get_people_detector_status()
         self.whatsapp_phone_number = s.get_phone_number_client()
 
-
-        
-    def prepare_server(self):
+    def prepare_server(self) -> None:
         """
         Method to prepare TCPServer from host and port to store connection object referencies.
         """
@@ -81,7 +81,7 @@ class TCPServer:
             self.show_initial_status()
         except skt.error as error:
             print(str(error))
-    
+
     def show_initial_status(self) -> None:
         """
         Show on console initial server status
@@ -92,8 +92,8 @@ class TCPServer:
         print_log('i', f"Motion detector enabled  : {self.status_motion_detector}")
         print_log('i', f"Streaming enabled        : {self.status_stream}")
         print_log('i', f"Whatsapp number to notif : {self.whatsapp_phone_number}")
-    
-    def run(self):
+
+    def run(self) -> None:
         """
         Method to keep alive waiting to more connections.
         """
@@ -106,53 +106,50 @@ class TCPServer:
                     id_uuid4=ident,
                     connector=connector,
                     address=address,
-                    # time_info=(get_time(),get_date()),
                     tcp_server=self
                 )
                 self.connections[ident].start()
+            except skt.error as _e_:
+                print('socket error', _e_)
             except KeyboardInterrupt:
                 self.tcp_server_running = False
                 self.stop_all_connections()
                 self.close_detectors()
                 print_log('i', "Server turned-off from keyboard")
-    
-    def reg_connections(self, id_camera):
+
+    def reg_connections(self, id_camera) -> None:
         """
         Log if a camera has not been connected prevoiusly.
         """
         self.id_cameras.add(id_camera)
 
-    def close_detectors(self)->None:
-        """To reg turned off detectors"""
-        
+    def close_detectors(self) -> None:
+        """
+        To reg turned off detectors from config file
+        """
         s.set_fire_detector_status(enabled="false")
         s.set_motion_detector_status(enabled="false")
         s.set_people_detector_status(enabled="false")
         s.set_stream_enabled(enabled="false")
-        
+
     def is_connected(self, id_camera):
         """
         Check if id_camera is already in actual connections.
         """
         return id_camera in self.id_cameras
-        # for key in self.connections:
-        #     if self.connections[key].cam_id == id_camera:
-        #         return True
-        # return False
 
-    def stop_all_connections(self):
+    def stop_all_connections(self) -> bool:
         """
         Stop all connections that have been created.
         """
         for connection in self.connections.values():
-
             if connection.running:
-                connection.join()
+                connection.stop_detectors()
                 connection.stop_connection()
-                # print(connection)
+                connection.join()
         print_log('i', "Stop all running connections")
-    
-    def print_number_of_connections(self):
+        
+    def print_number_of_connections(self) -> None:
         """
         Method to print logging number of connections.
         """
@@ -162,16 +159,15 @@ class TCPServer:
         #     cont = cont + 1
         print_log('i', f'Number of Connections : {len(self.connections)}')
 
-    def delete_id_camera(self,id_camera:str, uuid_camera:str):
+    def delete_id_camera(self, id_camera:str, uuid_camera:str):
         """
         Method to delete id camera connection.
         """
         if id_camera in self.id_cameras:
-            # self.connections[uuid_camera].join() #kill thread
             self.connections.pop(uuid_camera, None)
             self.id_cameras.remove(id_camera)
-    
-    def get_connections_info(self, actual_cam_id:str)->list:
+
+    def get_connections_info(self, actual_cam_id:str) -> list:
         """
         Method to get get connections info
         """
